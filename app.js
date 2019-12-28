7579a70fde4659d230b925fc7826e5ec0ac31993
[Gabriela] <gabriela.sikora@code.berlin>
2019-12-28 13:55:10 +0100

    Add darksky api and display live temp for current location

---
+++ app.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(
+        ".temperature-description"
+        );
+    let temperatureDegree = document.querySelector(
+        ".temperature-degree"
+        );
+    let locationTimezone = document.querySelector(
+        ".location-timezone"
+        );
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/8fc0a973af29f71e48211d7b7b05b468/${lat},${long}`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const { temperature, summary } = data.currently;
+                // Set DOM elements from the API
+                temperatureDegree.textContent = temperature;
+                
+            })
+        });
+    }
+
+
+});
